b6a012a8c5b85b0069ea04c27881117f0594f096
husercar <carlo.huser@gmail.com>
2023-10-05 15:45:01 +0200

    Fixed Batchfile

--- download_osm.bat
+++ download_osm.bat
@@ -3,37 +3,16 @@ setlocal enabledelayedexpansion
 
 REM Define default values for parameters
 set "db=gisdb"
-set "username=gisdb"
-set "password="
+set "username=postgres"
 
 REM Check for and process command line arguments
 if not "%~1"=="" set "db=%~1"
 if not "%~2"=="" set "username=%~2"
-if not "%~3"=="" set "password=%~3"
 
 REM Check if default.style file exists in the Downloads folder
 if not exist "%userprofile%\Downloads\default.style" (
     echo Downloading default.style...
     powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://learnosm.org/files/default.style', '%userprofile%\Downloads\default.style')"
-)
-
-REM Check if .pgpass file exists, and if not, create it with the provided password
-if not exist "%userprofile%\.pgpass" (
-    echo Creating .pgpass file...
-    echo localhost:5432:*:%username%:%password% > "%userprofile%\.pgpass"
-)
-
-REM Check if pg_hba.conf contains the necessary line, and if not, add it
-set "pg_hba_file=C:\Program Files\PostgreSQL\16\data\pg_hba.conf"
-set "auth_line=host    all             %username%           127.0.0.1/32            md5"
-findstr /C:"%auth_line%" "%pg_hba_file%" >nul || (
-    echo Adding line to pg_hba.conf...
-    echo %auth_line% >> "%pg_hba_file%"
-
-    REM Restart PostgreSQL service
-    echo Restarting PostgreSQL service...
-    net stop postgresql-x64-16
-    net start postgresql-x64-16
 )
 
 REM Download Switzerland OSM data
