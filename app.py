73a39f81ef9f3ec16d0a626e7785b6bda18ffedb
looopic <carlo.huser@gmail.com>
2023-10-11 11:34:29 +0200

    edited flask

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request
+import geopandas
 
 app = Flask(__name__)
 
