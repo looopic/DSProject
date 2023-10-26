6ef3e43ff98d20ca4297188ab2a88627f3d48f85
husercar <carlo.huser@gmail.com>
2023-10-26 12:49:12 +0200

    Tryout folium

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 from flask import Flask, render_template, request
+import folium
 
 app = Flask(__name__)
 
@@ -13,33 +14,41 @@ def get_db_connection():
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    amenities=refresh()
+    countries=refresh()
     if request.method == 'POST':
         return get_amenity()
-    return render_template('index.html', amenities=amenities)
+    return render_template('index.html', countries=countries)
 
 def refresh():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT DISTINCT amenity FROM planet_osm_polygon;')
-    amenities = cur.fetchall()
+    cur.execute("SELECT * FROM planet_osm_polygon WHERE admin_level='1';")
+    entries= cur.fetchall()
     cur.close()
     conn.close()
-    return amenities
+    return entries
 
 @app.route('/amenity', methods=['GET','POST'])
 def get_amenity():
     selected_amenity = request.form['amenity']
-    query_st='SELECT * FROM planet_osm_polygon WHERE amenity=\''+selected_amenity+'\';'
+    query_st='SELECT * FROM planet_osm_polygon WHERE osm_id=\''+selected_amenity[0]+'\';'
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT DISTINCT amenity FROM planet_osm_polygon;')
-    amenities = cur.fetchall()
+    cur.execute("SELECT * FROM planet_osm_polygon WHERE admin_level='1';")
+    countries = cur.fetchall()
     cur.execute(query_st)
     query=cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('index.html', amenities=amenities, query=query)
+    
+    m = folium.Map()
+
+    # set the iframe width and height
+    m.get_root().width = "800px"
+    m.get_root().height = "600px"
+    iframe = m.get_root()._repr_html_()
+
+    return render_template('index.html', countries=countries, query=query, country=selected_amenity, iframe=iframe)
 
 if __name__ == '__main__':
     app.run()
