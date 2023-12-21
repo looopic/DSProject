e9b76f6964dda6cab58e903c9c6c8918ce17d33e
Oliver Tanno <tannooli@students.zhaw.ch>
2023-12-21 22:08:32 +0100

    Add new comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Import Libraries
 import os
 import psycopg2
 from flask import Flask, render_template, request, redirect, url_for, jsonify
@@ -5,9 +6,10 @@ import folium
 import geopandas as gpd
 import wikipedia
 
-os.environ["DB_NAME"] = ""
-os.environ["DB_USERNAME"] = ""
-os.environ["DB_PASSWORD"] = ""
+# Set up environment variables
+os.environ["DB_NAME"] = "dsc_project"
+os.environ["DB_USERNAME"] = "postgres"
+os.environ["DB_PASSWORD"] = "MotorolaV500."
 
 
 app = Flask(__name__)
@@ -103,19 +105,56 @@ def refresh():
 @app.route("/country", methods=["GET", "POST"])
 def get_country():
     selected_country = request.form["country"][1:-1].split(",")
-    query_st = ("SELECT * FROM planet_osm_polygon WHERE admin_level='8' AND ST_CONTAINS("+ selected_country[1]+ ",way) ORDER BY name;")
+    query_st = (
+        "SELECT * FROM planet_osm_polygon WHERE admin_level='8' AND ST_CONTAINS("
+        + selected_country[1]
+        + ",way) ORDER BY name;"
+    )
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("SELECT osm_id, name,ST_UNION(array_agg(way)) AS way_union FROM planet_osm_polygon WHERE admin_level='4' AND ST_CONTAINS("+ selected_country[1]+ ",way) AND building IS NULL GROUP BY name, osm_id;")
+    cur.execute(
+        "SELECT osm_id, name,ST_UNION(array_agg(way)) AS way_union FROM planet_osm_polygon WHERE admin_level='4' AND ST_CONTAINS("
+        + selected_country[1]
+        + ",way) AND building IS NULL GROUP BY name, osm_id;"
+    )
     adminLevel4 = cur.fetchall()
     cur.close()
     gdf = gpd.GeoDataFrame.from_postgis(query_st, conn, geom_col="way")
-    water_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(water.geom,"+selected_country[1]+") FROM water;", conn, geom_col="geom")
-    forest_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(forest.geom,"+selected_country[1]+") FROM forest;", conn, geom_col="geom")
-    building_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(building.geom,"+selected_country[1]+") FROM building;", conn, geom_col="geom")
-    subdiv_gdf = gpd.GeoDataFrame.from_postgis("SELECT *, ST_Area(way)/1000000 AS area, water_area/(ST_Area(way)/1000000)*100 as per_water,forest_area/(ST_Area(way)/1000000)*100 as per_forest,building_area/(ST_Area(way)/1000000)*100 as per_building FROM sudvivision WHERE ST_CONTAINS("+selected_country[1]+",way);",conn,geom_col="way",index_col="osm_id",).to_dict("records")
+    water_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(water.geom," + selected_country[1] + ") FROM water;",
+        conn,
+        geom_col="geom",
+    )
+    forest_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(forest.geom," + selected_country[1] + ") FROM forest;",
+        conn,
+        geom_col="geom",
+    )
+    building_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(building.geom,"
+        + selected_country[1]
+        + ") FROM building;",
+        conn,
+        geom_col="geom",
+    )
+    subdiv_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT *, ST_Area(way)/1000000 AS area, water_area/(ST_Area(way)/1000000)*100 as per_water,forest_area/(ST_Area(way)/1000000)*100 as per_forest,building_area/(ST_Area(way)/1000000)*100 as per_building FROM sudvivision WHERE ST_CONTAINS("
+        + selected_country[1]
+        + ",way);",
+        conn,
+        geom_col="way",
+        index_col="osm_id",
+    ).to_dict("records")
     conn.close()
-    return render_template("country.html",country=selected_country,iframe=get_map(gdf, water_gdf, forest_gdf, building_gdf),wiki=get_wiki(selected_country[0]),subdivisions=subdiv_gdf,adminLevel4=adminLevel4,)
+    return render_template(
+        "country.html",
+        country=selected_country,
+        iframe=get_map(gdf, water_gdf, forest_gdf, building_gdf),
+        wiki=get_wiki(selected_country[0]),
+        subdivisions=subdiv_gdf,
+        adminLevel4=adminLevel4,
+    )
+
 
 # Display of subdivision's details
 @app.route("/subdivision", methods=["GET", "POST"])
@@ -124,31 +163,98 @@ def get_subdiv():
     print(selected_level[0:2])
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("SELECT name, ST_Union(way) AS aggregated_way FROM planet_osm_polygon WHERE admin_level='8'  AND ST_CONTAINS( ( SELECT ST_Union(array_agg(way))  FROM planet_osm_polygon  WHERE admin_level='4' AND name="+ selected_level[1]+ "),  way ) GROUP BY name ORDER BY name;")
+    cur.execute(
+        "SELECT name, ST_Union(way) AS aggregated_way FROM planet_osm_polygon WHERE admin_level='8'  AND ST_CONTAINS( ( SELECT ST_Union(array_agg(way))  FROM planet_osm_polygon  WHERE admin_level='4' AND name="
+        + selected_level[1]
+        + "),  way ) GROUP BY name ORDER BY name;"
+    )
     adminLevel8 = cur.fetchall()
     cur.close()
-    gdf = gpd.GeoDataFrame.from_postgis("SELECT * FROM planet_osm_polygon WHERE admin_level='8' AND ST_CONTAINS("+ selected_level[1]+ ",way) ORDER BY name;", conn, geom_col="way")
-    water_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(water.geom,"+selected_level[1]+") FROM water;", conn, geom_col="geom")
-    forest_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(forest.geom,"+selected_level[1]+") FROM forest;", conn, geom_col="geom")
-    building_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(building.geom,"+selected_level[1]+") FROM building;", conn, geom_col="geom")
-    community_gdf = gpd.GeoDataFrame.from_postgis("SELECT *, ST_Area(way)/1000000 AS area, water_area/(ST_Area(way)/1000000)*100 as per_water,forest_area/(ST_Area(way)/1000000)*100 as per_forest,building_area/(ST_Area(way)/1000000)*100 AS per_building, CASE WHEN EXISTS(SELECT ST_CONTAINS(c.way, r.way) FROM communities c, planet_osm_point r WHERE r.railway='Station') THEN 'YES' ELSE 'NO' END AS railway FROM communities WHERE ST_CONTAINS("+selected_level[1]+",way);",conn,geom_col="way",index_col="osm_id",).to_dict("records")
+    gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT * FROM planet_osm_polygon WHERE admin_level='8' AND ST_CONTAINS("
+        + selected_level[1]
+        + ",way) ORDER BY name;",
+        conn,
+        geom_col="way",
+    )
+    water_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(water.geom," + selected_level[1] + ") FROM water;",
+        conn,
+        geom_col="geom",
+    )
+    forest_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(forest.geom," + selected_level[1] + ") FROM forest;",
+        conn,
+        geom_col="geom",
+    )
+    building_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(building.geom,"
+        + selected_level[1]
+        + ") FROM building;",
+        conn,
+        geom_col="geom",
+    )
+    community_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT *, ST_Area(way)/1000000 AS area, water_area/(ST_Area(way)/1000000)*100 as per_water,forest_area/(ST_Area(way)/1000000)*100 as per_forest,building_area/(ST_Area(way)/1000000)*100 AS per_building, CASE WHEN EXISTS(SELECT ST_CONTAINS(c.way, r.way) FROM communities c, planet_osm_point r WHERE r.railway='Station') THEN 'YES' ELSE 'NO' END AS railway FROM communities WHERE ST_CONTAINS("
+        + selected_level[1]
+        + ",way);",
+        conn,
+        geom_col="way",
+        index_col="osm_id",
+    ).to_dict("records")
     conn.close()
-    return render_template("subdiv.html", selected_level=selected_level, adminLevel8=adminLevel8,iframe=get_map(gdf, water_gdf, forest_gdf, building_gdf), wiki=get_wiki(selected_level[0]), communities=community_gdf)
+    return render_template(
+        "subdiv.html",
+        selected_level=selected_level,
+        adminLevel8=adminLevel8,
+        iframe=get_map(gdf, water_gdf, forest_gdf, building_gdf),
+        wiki=get_wiki(selected_level[0]),
+        communities=community_gdf,
+    )
+
 
 # Display of community's details
-@app.route("/community", methods=["GET","POST"])
+@app.route("/community", methods=["GET", "POST"])
 def get_community():
     selected_community = request.form["community"][1:-1].split(",")
     conn = get_db_connection()
-    gdf = gpd.GeoDataFrame.from_postgis("SELECT * FROM planet_osm_polygon WHERE admin_level='8' AND ST_CONTAINS("+ selected_community[1]+ ",way) ORDER BY name;", conn, geom_col="way")
-    water_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(water.geom,"+selected_community[1]+") FROM water;", conn, geom_col="geom")
-    forest_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(forest.geom,"+selected_community[1]+") FROM forest;", conn, geom_col="geom")
-    building_gdf = gpd.GeoDataFrame.from_postgis("SELECT ST_Intersection(building.geom,"+selected_community[1]+") FROM building;", conn, geom_col="geom")
+    gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT * FROM planet_osm_polygon WHERE admin_level='8' AND ST_CONTAINS("
+        + selected_community[1]
+        + ",way) ORDER BY name;",
+        conn,
+        geom_col="way",
+    )
+    water_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(water.geom," + selected_community[1] + ") FROM water;",
+        conn,
+        geom_col="geom",
+    )
+    forest_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(forest.geom,"
+        + selected_community[1]
+        + ") FROM forest;",
+        conn,
+        geom_col="geom",
+    )
+    building_gdf = gpd.GeoDataFrame.from_postgis(
+        "SELECT ST_Intersection(building.geom,"
+        + selected_community[1]
+        + ") FROM building;",
+        conn,
+        geom_col="geom",
+    )
     conn.close()
-    return render_template("community.html", selected_community=selected_community, iframe=get_map(gdf,water_gdf,forest_gdf,building_gdf), wiki=get_wiki(selected_community[0]))
+    return render_template(
+        "community.html",
+        selected_community=selected_community,
+        iframe=get_map(gdf, water_gdf, forest_gdf, building_gdf),
+        wiki=get_wiki(selected_community[0]),
+    )
+
 
 # Oliver's Part
-# refresh function: selects unique amenities
+# Querry to selects unique values from column amenities
 def refresh_amenity():
     conn = get_db_connection()
     cur = conn.cursor()
@@ -159,29 +265,25 @@ def refresh_amenity():
     return amenity_values
 
 
-# Query page to select amenities
+# Refresh with the selected amenities
 @app.route("/querrys", methods=["GET"])
 def querrys():
     amenity_values = refresh_amenity()
     return render_template("querrys.html", amenity_values=amenity_values)
 
 
+# Querry for the database with the selected amenities to show the results of it
 @app.route("/result", methods=["POST"])
 def process_query():
     try:
         selected_amenities = request.json.get("selectedAmenities")
-
         if not selected_amenities:
             return jsonify([])
-
-        # Build the SQL query with OR conditions for selected amenities
         query = "SELECT osm_id, building, 'addr:housename', name, ST_Area(way) AS way_area, way FROM planet_osm_polygon WHERE amenity IN ("
         query += ", ".join(["%s" for _ in selected_amenities])
         query += ");"
-
         conn = get_db_connection()
         cur = conn.cursor()
-
         cur.execute(query, selected_amenities)
         results = cur.fetchall()
 
@@ -192,15 +294,16 @@ def process_query():
             conn.close()
 
 
+# Query page to select amenities
 @app.route("/result", methods=["GET"])
 def show_results():
     return redirect(url_for("querrys"))
 
 
+# Selection of predifined queries for the dropdown field
 @app.route("/predefined_query", methods=["POST"])
 def predefined_query():
     selected_query = request.json.get("selectedQuery")
-
     if selected_query == "historicalChimneys":
         query = "SELECT osm_id, building, historic, man_made, ST_Area(way) AS way_area, way FROM planet_osm_polygon WHERE historic = 'industrial' AND man_made = 'chimney';"
     elif selected_query == "commercialBuildings":
